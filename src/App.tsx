c8d38e92f387b1e522fa9d28f1b30e733e159aa2
mm1995tk <jimi.woodstock1995@gmail.com>
2023-11-16 01:35:27 +0900

    画像を選択できる

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageSourcePropType, StyleSheet, Text, View } from "react-native";
 import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 export default function App() {
+  const [selectedPhoto, setPhoto] = useState<ImageSourcePropType>();
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setPhoto(result.assets[0]);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
   return (
     <View style={styles.container}>
       <ImageViewer
         placeholderImageSource={{
           uri: "https://docs.expo.dev/static/images/tutorial/background-image.png",
         }}
+        selected={selectedPhoto}
       />
-      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+      <View style={styles.footerContainer}>
+        <Button
+          label="Choose a photo"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,5 +46,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -2,10 +2,14 @@ import { StyleSheet, Image, ImageSourcePropType } from "react-native";
 
 export default function ImageViewer({
   placeholderImageSource,
+  selected,
 }: {
   placeholderImageSource: ImageSourcePropType;
+  selected?: ImageSourcePropType;
 }) {
-  return <Image source={placeholderImageSource} style={styles.image} />;
+  return (
+    <Image source={selected ?? placeholderImageSource} style={styles.image} />
+  );
 }
 
 const styles = StyleSheet.create({
